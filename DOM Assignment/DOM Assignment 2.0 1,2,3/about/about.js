cee25e203b404b1d7b73912bb94e8fd6408cb0ae
Debangshu <debangshu.801@gmail.com>
2023-02-26 00:06:03 +0530

    add dom 2,3,4 and 5 projects

--- DOM Assignment/DOM Assignment 2.0 1,2,3/about/about.js
+++ DOM Assignment/DOM Assignment 2.0 1,2,3/about/about.js
@@ -1,3 +1,4 @@
+
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -8,4 +9,34 @@ accordian.forEach((element) => {
       para.style.display = "block";
     }
   });
+});
+//Task 01
+const project=document.querySelector("header > nav > ul");
+project.querySelectorAll("li")[2].innerText="Projects";
+
+const color=document.querySelectorAll(".accordian > h3");
+console.log([...color].map(cc =>cc.style.backgroundColor='lightblue'));
+//Task 02
+
+const div=document.createElement('div');
+div.setAttribute("class","accordian");
+const h1=document.createElement('h3');
+h1.innerText="Skills";
+const p=document.createElement('p');
+p.innerText='I posses very good command over the Full Stack Development technologies like MERN which can seen in my work over Git Hub.';
+const pos=document.querySelector(".accordian-wrapper");
+pos.appendChild(div);
+div.append(h1,p);
+// console.log(div.appendChild(h1));
+const colorfive=document.querySelectorAll(".accordian > h3")[4];
+colorfive.setAttribute("style","backgroundColor: red");
+
+
+colorfive.addEventListener("click", () => {
+  let para = colorfive.nextElementSibling;
+  if (para.style.display === "block") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
 });
